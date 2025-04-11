cb64a8e9cde68f2754eeadef898ea2b6ac650a70
Bartosz Łężniak <146667244+lezniakb@users.noreply.github.com>
2025-04-11 14:13:03 +0200

    Checkpoint

--- mmc_fat/src/main.c
+++ mmc_fat/src/main.c
@@ -12,7 +12,6 @@
 #include "lpc17xx_pinsel.h"		/* ustwaienie pinów */
 #include "lpc17xx_gpio.h"		/* GPIO */
 #include "lpc17xx_ssp.h"		/* SPI, uzywane do karty SD */
-#include "lpc17xx_uart.h"		/* UART */
 #include "lpc17xx_timer.h"		/* timer do czasu */
 #include "stdio.h"		        /* biblioteka We/Wy */
 #include "lpc17xx_adc.h"
@@ -21,52 +20,26 @@
 #include "ff.h"
 
 #include "oled.h"			/* wczytana biblioteka do obslugi ekranu OLED */
-#include "acc.h"
-
-#define UART_DEV LPC_UART3		/* definicja domyslnego UART */
-
-static volatile uint8_t oledEnabled = 1;    /* flaga stanu OLED (On/Off)*/
+#include <stdbool.h>
 
 static FILINFO Finfo;		/* zasob przechowujacy informacje o plikach z karty SD */
 static FATFS Fatfs[1];		/* implementacja fatfs do wczytywania zasobu FAT */
 static uint8_t buf[64];		/* obecnie nieuzywany bufor UART */
 
-static void init_uart(void)		/* init UART */
-{
-	PINSEL_CFG_Type PinCfg;
-	UART_CFG_Type uartCfg;
-
-	/* Initialize UART3 pin connect */		/* konfiguruje UART3 na P0.0 i P0.1 */
-	PinCfg.Funcnum = 2;
-	PinCfg.Pinnum = 0;
-	PinCfg.Portnum = 0;
-	PINSEL_ConfigPin(&PinCfg);
-	PinCfg.Pinnum = 1;
-	PINSEL_ConfigPin(&PinCfg);
-
-	uartCfg.Baud_rate = 115200;		/* ustawia prędkość transmisji */
-	uartCfg.Databits = UART_DATABIT_8;		/* ustawia 8 bitów danych */
-	uartCfg.Parity = UART_PARITY_NONE;		/* ustawia brak parzystości */
-	uartCfg.Stopbits = UART_STOPBIT_1;		/* ustawia 1 bit stopu */
-
-	UART_Init(UART_DEV, &uartCfg);		/* inicjalizuje UART */
-
-	UART_TxCmd(UART_DEV, ENABLE);		/* włącza transmisję */
-
-}
-
+/* zapisywanie danych o znalezionych plikach na karcie SD */
+#define MAX_FILES 9
+#define MAX_FILENAME_LEN 64
 
 static void init_ssp(void)		/* inicjalizacja interfejsu SPI */
 {
-	SSP_CFG_Type SSP_ConfigStruct;		/* deklaracja struktury konfiguracyjej dla SPI */
-	PINSEL_CFG_Type PinCfg;				/* i pinów */
+	SSP_CFG_Type SSP_ConfigStruct;		/* konfiguracja SPI */
+	PINSEL_CFG_Type PinCfg;				/* konfiguracja pinow */
 
 	/*
-	 * Initialize SPI pin connect
-	 * P0.7 - SCK (zegar SPI)
-	 * P0.8 - MISO (dane odczytywane)
-	 * P0.9 - MOSI (dane wysyłane)
-	 * P2.2 ustawiony jako GPIO dla SSEL (chip select)
+	 * P0.7 SCK (zegar SPI)
+	 * P0.8 MISO (odczyt)
+	 * P0.9 MOSI (wyslanie danych)
+	 * P2.2 Ustawiony jako GPIO dla ssel (wybor chip)
 	 */
 	PinCfg.Funcnum = 2;
 	PinCfg.OpenDrain = 0;
@@ -83,28 +56,25 @@ static void init_ssp(void)		/* inicjalizacja interfejsu SPI */
 	PinCfg.Pinnum = 2;
 	PINSEL_ConfigPin(&PinCfg);
 
-	SSP_ConfigStructInit(&SSP_ConfigStruct);		/* inicjalizuje SPI i włącza go (kod do końca) */
-
-	// Initialize SSP peripheral with parameter given in structure above
+	/* init SPI i uruchamia */
+	SSP_ConfigStructInit(&SSP_ConfigStruct);
 	SSP_Init(LPC_SSP1, &SSP_ConfigStruct);
-
-	// Enable SSP peripheral
 	SSP_Cmd(LPC_SSP1, ENABLE);
 
 }
 
-void SysTick_Handler(void) {		/* obsługa przerwania SysTick */
+void SysTick_Handler(void) {
+	/* obsluga przerwan */
     disk_timerproc();
 }
 
-
-// inicjalizacja oled_periph
 
 static uint32_t msTicks = 0;
 
 
 static uint32_t getTicks(void)
 {
+	/* pobiera wartosc przerwania systemowego */
     return msTicks;
 }
 
@@ -113,7 +83,7 @@ static void init_i2c(void)
 {
 	PINSEL_CFG_Type PinCfg;
 
-	/* Initialize I2C2 pin connect */
+	/* I2C2 pin connect */
 	PinCfg.Funcnum = 2;
 	PinCfg.Pinnum = 10;
 	PinCfg.Portnum = 0;
@@ -121,10 +91,8 @@ static void init_i2c(void)
 	PinCfg.Pinnum = 11;
 	PINSEL_ConfigPin(&PinCfg);
 
-	// Initialize I2C peripheral
+	// inicjalizacja i uruchomienie
 	I2C_Init(LPC_I2C2, 100000);
-
-	/* Enable I2C1 operation */
 	I2C_Cmd(LPC_I2C2, ENABLE);
 }
 
@@ -133,9 +101,8 @@ static void init_adc(void)
 	PINSEL_CFG_Type PinCfg;
 
 	/*
-	 * Init ADC pin connect
-	 * AD0.0 on P0.23
-	 */
+	 * Inicjalizacja ADC i laczenie pinow
+	 * AD0.0 na P0.23 */
 	PinCfg.Funcnum = 1;
 	PinCfg.OpenDrain = 0;
 	PinCfg.Pinmode = 0;
@@ -143,36 +110,31 @@ static void init_adc(void)
 	PinCfg.Portnum = 0;
 	PINSEL_ConfigPin(&PinCfg);
 
-	/* Configuration for ADC :
-	 * 	Frequency at 0.2Mhz
-	 *  ADC channel 0, no Interrupt
+	/* Konfiguracja ADC:
+	 * Częstotliwość 0.2Mhz
+	 * ADC kanał 0, bez przerwań
 	 */
 	ADC_Init(LPC_ADC, 200000);
 	ADC_IntConfig(LPC_ADC,ADC_CHANNEL_0,DISABLE);
 	ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_0,ENABLE);
 
 }
-/* zobaczymy potem
-void button_init(void) {
-    PINSEL_CFG_Type PinCfg;
 
-    // Konfiguracja pinu P0.4 jako GPIO:
-    // - Port 0, Pin 4
-    // - Funkcja 0 - czyli GPIO (domyślna)
-    // - Pinmode ustawiony na 0, co zwykle oznacza aktywację rezystora pull-up
-    // - OpenDrain wyłączony (0) - używamy trybu push-pull
-    PinCfg.Portnum   = 0;
-    PinCfg.Pinnum    = 4;
-    PinCfg.Funcnum   = 0;       // wybieramy funkcję GPIO
-    PinCfg.Pinmode   = 0;       // pull-up włączony (możesz zmienić w zależności od sprzętu)
+static void button_init(void)
+{
+    /* P0.4 jako wejscie GPIO */
+    PINSEL_CFG_Type PinCfg;
+    PinCfg.Funcnum = 0;
     PinCfg.OpenDrain = 0;
+    PinCfg.Pinmode = 0; // pullup
+    PinCfg.Pinnum = 4;
+    PinCfg.Portnum = 0;
     PINSEL_ConfigPin(&PinCfg);
 
-    // Ustawienie kierunku pinu jako wejście
-    // Trzeci argument równy 0 oznacza konfigurację jako wejście
-    GPIO_SetDir(0, (1 << 4), 0);
+    /* ustaw P0.4 jako input */
+    GPIO_SetDir(0, 1 << 4, 0);
 }
-*/
+
 
 int main (void) {
     /* ------ deklaracja podstawowych zmiennych ------ */
@@ -183,36 +145,22 @@ int main (void) {
     BYTE res, b1;   /* zmienne pomocnicze (wynik operacji lub status funckji)*/
     DIR dir;        /* wczytany katalog z karty SD */
 
-    /* ------ zmienne czujnikow i korekcji ------ */
-    /* ustawienia do obslugi akcelerometru oraz korekty odczytow */
-    int32_t xoff = 0;
-    int32_t yoff = 0;
-    int32_t zoff = 0;
-
-    int8_t x = 0;
-    int8_t y = 0;
-    int8_t z = 0;
-
-    int32_t t = 0;
-    uint32_t lux = 0;
-    uint32_t trim = 0;
-
     /* ------ inicjalizacja peryferiow i urzadzen ------ */
     init_ssp();     /* SPI - komunikacja z karta SD */
-    //init_uart(); nieuzywana inicjalizacja UART'u
-
     init_i2c();     /* I2C - komunikacja z OLEDem */
     init_adc();     /* ADC - konfiguracja wejsc analogowych */
 
-    /* ------ inicjalizacja modulow wyswietlacza OLED i czujnikow ------ */
+    /* ------ inicjalizacja modulow wyswietlacza OLED i przycisku ------ */
     oled_init();    /* wyswietlacz OLED */
-    light_init();   /* czujnik swiatla (do kontroli natezenia) */
-    acc_init();     /* akcelerometr (przyspieszenia) */
     button_init();  /* przycisk - pozwala na wlaczenie/wylaczenie ekranu OLED */
 
     /* ------ rozpoczecie dzialania ------ */
     /* iterator potrzebny do operacji */
     int i = 0;
+
+    /* alokacja pamieci do zapisu nazw oraz ilosc zapisanych plikow*/
+    char fileList[MAX_FILES][MAX_FILENAME_LEN];
+    int fileCount = 0;
 
     /* uwaga: działająca karta SD to niebieska 128, sformatowana do FAT, najniższy rozmiar alokacji bloku */
     
@@ -239,37 +187,37 @@ int main (void) {
         return 1;
     }
   
-    oled_putString(1, 9, (uint8_t*)"Inicjalizacja OK", OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+    oled_putString(1, 9, (uint8_t*)"Init OK", OLED_COLOR_BLACK, OLED_COLOR_WHITE);
 
     /* ------ wyswietlenie danych dot. wykrytej karty SD ------ */
     /* wyswietla liczbe sektorow */
     if (disk_ioctl(0, GET_SECTOR_COUNT, &p2) == RES_OK) {
-        i = sprintf((char*)buf, "Sektorow: %d \r\n", p2);
+        i = sprintf((char*)buf, "ilo sek: %d \r\n", p2);
         oled_putString(1, 18, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
     }
 
     /* wyswietla rozmiar pojedynczego sektora */
     if (disk_ioctl(0, GET_SECTOR_SIZE, &w1) == RES_OK) {
-        i = sprintf((char*)buf, "Rozmiar sektora: %d \r\n", w1);
+        i = sprintf((char*)buf, "rozm sek: %d \r\n", w1);
         oled_putString(1, 27, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
     }
 
     /* wyswietla wielkosc bloku, czyli liczbe sektorow w jednym bloku */
     if (disk_ioctl(0, GET_BLOCK_SIZE, &p2) == RES_OK) {
-        i = sprintf((char*)buf, "Wielkosc bloku: %d \r\n", p2);
+        i = sprintf((char*)buf, "rozm blok: %d \r\n", p2);
         oled_putString(1, 36, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
     }
 
     /* wyswietla typ karty zdefiniowana przez sterownik */
     if (disk_ioctl(0, MMC_GET_TYPE, &b1) == RES_OK) {
-        i = sprintf((char*)buf, "Typ SD: %d \r\n", b1);
+        i = sprintf((char*)buf, "SD typ: %d \r\n", b1);
         oled_putString(1, 45, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
     }
 
     /* zamontowanie systemu plikow FAT */
     res = f_mount(0, &Fatfs[0]);
     if (res != FR_OK) {
-        i = sprintf((char*)buf, "Blad montowania: %d \r\n", res);
+        i = sprintf((char*)buf, "montow. err: %d \r\n", res);
         oled_putString(1, 54, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
         return 1;
     }
@@ -277,51 +225,68 @@ int main (void) {
     /* otwiera katalog główny karty SD */
     res = f_opendir(&dir, "/");
     if (res) {
-        i = sprintf((char*)buf, "Blad otwierania /: %d \r\n", res);
+        i = sprintf((char*)buf, "otw. err /: %d \r\n", res);
         oled_putString(1, 63, (uint8_t*)buf, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
         return 1;
     }
 
-    oled_clearScreen(OLED_COLOR_BLACK);
     /* ------ operacje na karcie SD ------ */
-    /* zbiera i wyswietla pliki znalezione na karcie SD (max 9) */
-    for(int i = 1; i < 10; i++) {
+    /* zbiera, zapisuje i wyswietla pliki znalezione na karcie SD */
+    fileCount = 0;
+    for (int i = 0; i < MAX_FILES; ) {
         res = f_readdir(&dir, &Finfo);
-        if (Finfo.fname[0] == '_' || (Finfo.fattrib & AM_DIR)) {
-            i--;
-            continue;
-        }
         if ((res != FR_OK) || !Finfo.fname[0]) break;
-        oled_putString(1, 1 + i * 8, Finfo.fname, OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-    };
 
-    /* na razie tego nie tykalem XD */
-    acc_read(&x, &y, &z);
-    xoff = 0-x;
-    yoff = 0-y;
-    zoff = 64-z;
+        /* pomin foldery i ukryte pliki */
+        if (Finfo.fattrib & AM_DIR || Finfo.fname[0] == '_') continue;
 
-    light_enable();
+        /* zapisz nazwe pliku do pamieci */
+        snprintf(fileList[fileCount], MAX_FILENAME_LEN, "%s", Finfo.fname);
+        fileCount++;
+        i++;
+    }
 
-    /* NOWOŚĆ (eksperymentalne): uzywanie przycisku do wlaczania/wylaczania OLEDa (funkcjonalnosc 1)*/
-    /* jeszcze nie testowane. */
+    /* wyswietl wszystkie wczytane pliki */
+    oled_clearScreen(OLED_COLOR_WHITE);
+    for (int i = 0; i < fileCount; i++) {
+        oled_putString(1, 1 + (i * 8), (uint8_t*)fileList[i], OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+    }
 
-    uint8_t btn1 = 0;
 
-    GPIO_SetDir(0, 1<<4, 0);
+    /* obsluga wciskania przycisku */
+    bool screenState = true;       // true = visible, false = blank
+    bool lastButton = true;        // last button state
+    bool screenNeedsUpdate = false; // marks whether screen should be redrawn
 
-    btn1 = ((GPIO_ReadValue(0) >> 4) & 0x01);
+    /* sprawdzaj wywolanie przycisku w petli */
+    while (1)
+    {
+        /* odczytaj wartosc na P0.4 */
+        bool btn = (GPIO_ReadValue(0) & (1 << 4)) != 0;
 
-        if (btn1 == 0) {
-            oled_clearScreen(OLED_COLOR_WHITE);
-        	oled_putString(1, 9, (uint8_t*)"btn1 == 0", OLED_COLOR_WHITE, OLED_COLOR_BLACK);
-            Timer0_Wait(1000);
+        /* wykryj opadanie (wciskanie przycisku) */
+        if (!btn && lastButton)
+        {
+            screenState = !screenState;
+            screenNeedsUpdate = true;
         }
-        else {
-        	sprintf(str, "btn1 = %d", btn1);
-            oled_clearScreen(OLED_COLOR_BLACK);
-        	oled_putString(1, 9, (uint8_t*)str, OLED_COLOR_BLACK, OLED_COLOR_WHITE);
-            Timer0_Wait(1000);
+
+        /* wykonaj akcje tylko raz, przy zmianie stanu ekranu */
+        if (screenNeedsUpdate)
+        {
+            if (!screenState) {
+                oled_clearScreen(OLED_COLOR_BLACK);
+            } else {
+                oled_clearScreen(OLED_COLOR_WHITE);
+                for (int i = 0; i < fileCount; i++) {
+                    oled_putString(1, 1 + (i * 8), (uint8_t*)fileList[i], OLED_COLOR_BLACK, OLED_COLOR_WHITE);
+                }
+            }
+
+            /* reset flagi */
+            screenNeedsUpdate = false;
         }
-        Timer0_Wait(10);
+
+        lastButton = btn;
+    }
 }
